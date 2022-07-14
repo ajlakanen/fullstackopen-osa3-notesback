a29595d211d6544f423400a144425cdc74ba83a9
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-14 21:24:02 +0300

    Poistetaan `/` reitti

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ let notes = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello Worldss!</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello Worldss!</h1>");
+// });
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
